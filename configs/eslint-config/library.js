632dbfc01542e484022226c21fe6ed48d0cd297e
Danilo Romão <htpps.sw@gmail.com>
2025-02-27 16:25:20 -0300

    commit initial

---
+++ configs/eslint-config/library.js
@@ -0,0 +1,12 @@
+
+/**@type {import("eslint").Linter.Config}  */
+
+module.exports = {
+  extends: [
+    '@rocketseat/eslint-config/react'
+  ],
+  plugins: ['simple-import-sort'],
+  rule: {
+         'simple-import-sort/imports': 'error'
+  }
+}  
---
+++ configs/eslint-config/node.js
@@ -0,0 +1,12 @@
+
+/**@type {import("eslint").Linter.Config}  */
+
+module.exports = {
+     extends: [
+          '@rocketseat/eslint-config/node'
+     ],
+     plugins: ['simple-import-sort'],
+     rule: {
+          'simple-import-sort/imports': 'error'
+     }
+}
