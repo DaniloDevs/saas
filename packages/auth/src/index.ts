1521680305de141e2768f86039516befe3c7c781
Danilo Romão <htpps.sw@gmail.com>
2025-03-02 01:41:35 -0300

    Feat: create configs and package

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,28 @@
+import { createMongoAbility, ForcedSubject, CreateAbility, MongoAbility, AbilityBuilder } from '@casl/ability';
+import { permissions } from './permissions';
+import { User } from './models/user';
+
+const actions = ['manage', 'invite'] as const;
+const subjects = ['User', 'all'] as const;
+type AppAbilities = [
+     typeof actions[number],
+     typeof subjects[number] | ForcedSubject<Exclude<typeof subjects[number], 'all'>>
+];
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+
+export function defineAbilityFor(user: User) {
+     const builder = new AbilityBuilder(createAppAbility)
+
+     if (typeof permissions[user.role] !== 'function') {
+          throw new Error(`Permission for role ${user.role} not found.`)
+     }
+
+     permissions[user.role](user, builder)
+     
+     const ability = builder.build()
+
+     return ability
+}
---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,16 @@
+import { AbilityBuilder } from "@casl/ability"
+import { AppAbility } from "."
+import { User } from "./models/user"
+
+type Role = "ADIMIN" | "MEMBER"
+
+type PermisionByRole = (user: User, builder: AbilityBuilder<AppAbility>) => void
+
+export const permissions: Record<Role, PermisionByRole> = {
+     ADIMIN(_, { can }) {
+          can('manage', "all")
+     },
+     MEMBER(_, { can }) {
+          can('invite', 'User')
+     }
+}
