06c475ae7890a0943e70b5f58ba9f285d54f5b00
Danilo Romão <htpps.sw@gmail.com>
2025-03-11 00:16:55 -0300

    feat: create password recover

---
+++ apps/backend/src/http/routes/auth/reset-password.ts
@@ -0,0 +1,49 @@
+import type { FastifyInstance } from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { UnauthorizationError } from '../_errors/unauthorized';
+import { hash } from 'bcryptjs';
+
+export default async function ResetPassword(app: FastifyInstance) {
+     app
+          .withTypeProvider<ZodTypeProvider>()
+          .post(
+               '/password/recover',
+               {
+                    schema: {
+                         tags: ["Auth"],
+                         summary: "Password reset from user",
+                         body: z.object({
+                              code: z.string(),
+                              password: z.string().min(6),
+                         }),
+                         response: {
+                              201: z.null()
+                         }
+                    }
+               },
+               async (request, reply) => {
+                    const { code, password } = request.body
+
+                    const tokenFromCode = await prisma.token.findUnique({
+                         where: { id: code }
+                    })
+
+                    if (!tokenFromCode) {
+                         throw new UnauthorizationError()
+                    }
+
+                    const passwordHash = await hash(password, 6)
+
+                    await prisma.user.update({
+                         where: { id: tokenFromCode.id },
+                         data:{
+                              passwordHash
+                         }
+                    })
+
+                    return reply.status(204).send()
+               }
+          )
+}
--- apps/backend/src/http/routes/setup-routes.ts
+++ apps/backend/src/http/routes/setup-routes.ts
@@ -2,6 +2,8 @@ import type { FastifyInstance } from "fastify";
 import CreateAccount from "./auth/create-account";
 import AuthtenticateWithPassaword from "./auth/authenticate-with-password";
 import GetProfile from "./auth/get-profile";
+import RequestPasswordRecover from "./auth/request-password-recover";
+import ResetPassword from "./auth/reset-password";
 
 
 export default async function SetupRoutes(server: FastifyInstance) {
@@ -9,4 +11,6 @@ export default async function SetupRoutes(server: FastifyInstance) {
      server.register(CreateAccount)
      server.register(AuthtenticateWithPassaword)
      server.register(GetProfile)
+     server.register(RequestPasswordRecover)
+     server.register(ResetPassword)
 }
