b6750bf3506699efd7fcd339b911567cb9febaa4
Danilo Romão <https.sw@gmail.com>
2025-05-20 08:16:20 -0300

    Feet: add project list

--- apps/frontend/src/app/(app)/org/[slug]/(main)/page.tsx
+++ apps/frontend/src/app/(app)/org/[slug]/(main)/page.tsx
@@ -1,9 +1,37 @@
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
 
-export default function Home() {
+import { ability, getCurrentOrganization } from '@/auth/auth'
+import { Button } from '@/components/ui/button'
+
+import { ProjectList } from './project-list'
+
+export default async function Projects() {
+     const currentOrg = getCurrentOrganization()
+     const permissions = await ability()
+
      return (
-          <div className="py-4">
+          <div className="space-y-4">
+               <div className="flex items-center justify-between">
+                    <h1 className="text-2xl font-bold">Projects</h1>
 
-               <h1>home</h1>
+                    {permissions?.can('create', 'Project') && (
+                         <Button size="sm" asChild>
+                              <Link href={`/org/${currentOrg}/create-project`}>
+                                   <Plus className="mr-2 size-4" />
+                                   Create project
+                              </Link>
+                         </Button>
+                    )}
+               </div>
+
+               {permissions?.can('get', 'Project') ? (
+                    <ProjectList />
+               ) : (
+                    <p className="text-sm text-muted-foreground">
+                         You are not allowed to see organization projects.
+                    </p>
+               )}
           </div>
-     );
+     )
 }
